9320ab95eeaaedba81a8635fcc442935f3a31d2e
henryma26 <125942284+henryma26@users.noreply.github.com>
2023-03-06 01:36:59 +0800

    Update findstock.py

--- findstock.py
+++ findstock.py
@@ -11,6 +11,7 @@ import pandas as pd
 with open('hkstocksfutu_20230224.txt', 'r') as f:
     stocklist = f.read().splitlines()
 
+# data = pd.read_pickle('stocksmorethan900.pkl')
 
 ###find out the hk stocks that have enough data for me to analyze, mainly between 2020-2023
 def findstock():
@@ -24,4 +25,26 @@ def findstock():
             
     return stocksmorethan900
 
+###find out the hk stocks that were above 250sma or below 250sma
+def findstocksma(stocks):
+    above250sma = []
+    below250sma = []
+    
+    for stock in stocks[0:10]:
+        df_stock = pd.read_pickle(f'{stock}.pkl')
+        df_stock['250sma'] = df_stock['close'].rolling(window=250).mean()
+        
+        start_date = '2021-02-17 00:00:00'
+        start_close = df_stock.loc[df_stock['time_key'] == start_date, 'close'].values[0]
+        start_sma250 = df_stock.loc[df_stock['time_key'] == start_date, '250sma'].values[0]
+        if start_close > start_sma250:
+            above250sma.append(stock)
+        else:
+            below250sma.append(stock)
+        # print(start_close)
+
+    return df_stock, above250sma, below250sma
+
 data = findstock()
+
+bb = findstocksma(data)
