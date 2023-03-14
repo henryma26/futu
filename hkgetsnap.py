4b9afb50428cc2969c7994b0877f9d2829865394
henryma26 <125942284+henryma26@users.noreply.github.com>
2023-03-14 09:30:07 +0800

    Update hkgetsnap.py

--- hkgetsnap.py
+++ hkgetsnap.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Mar 10 14:42:19 2023
+Created on Mon Mar 13 13:16:14 2023
 
+@author: henryma
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 10 14:42:19 2023
 This file is used to capture the snapshot of the market realtime
 """
 
@@ -23,7 +29,7 @@ def checkstatus():
         stock_list.remove(stock)
     
     return stock_list
-\
+
     
 def get_snapshot(remain_hkstocks):
     stocks_data = []
@@ -38,12 +44,62 @@ def get_snapshot(remain_hkstocks):
         stocks_data.append(data)
         stockrange+=400
         count+=1
-        
+    
+    quote_ctx.close()
+    
     return stocks_data
 
+def myfilter(final):
+    my_filter = []
+    for index, row in final.iterrows():
+        if row['last_price'] > row['prev_close_price'] and row['turnover'] > 200000 and row['close_price_5min'] != 0:
+            my_filter.append(row['code'])
+    
+    return my_filter
+    
+def comparesma250(getstock):
+    # getstock = getstock[300:]
+    countstock = len(getstock)
+    numberofreset = round(len(getstock)/100)
+    quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)
+    
+    for stock in getstock[0:100]:
+        ret_sub, err_message = quote_ctx.subscribe(stock, [SubType.K_DAY], subscribe_push=False)
+        # 先订阅 K 线类型。订阅成功后 FutuOpenD 将持续收到服务器的推送，False 代表暂时不需要推送给脚本
+        if ret_sub == RET_OK:  # 订阅成功
+            ret, data = quote_ctx.get_cur_kline(stock, 2, KLType.K_DAY, AuType.QFQ)  # 获取港股00700最近2个 K 线数据
+            if ret == RET_OK:
+                print(data)
+                # print(data['turnover_rate'][0])   # 取第一条的换手率
+                # print(data['turnover_rate'].values.tolist())   # 转为 list
+            else:
+                print('error:', data)
+        else:
+            print('subscription failed', err_message)
+    quote_ctx.close()
+    
+    quote_ctx = OpenQuoteContext(host='127.0.0.1', port=11111)
+    for stock in getstock[100:200]:
+        ret_sub, err_message = quote_ctx.subscribe(stock, [SubType.K_DAY], subscribe_push=False)
+        # 先订阅 K 线类型。订阅成功后 FutuOpenD 将持续收到服务器的推送，False 代表暂时不需要推送给脚本
+        if ret_sub == RET_OK:  # 订阅成功
+            ret, data = quote_ctx.get_cur_kline(stock, 2, KLType.K_DAY, AuType.QFQ)  # 获取港股00700最近2个 K 线数据
+            if ret == RET_OK:
+                print(data)
+                # print(data['turnover_rate'][0])   # 取第一条的换手率
+                # print(data['turnover_rate'].values.tolist())   # 转为 list
+            else:
+                print('error:', data)
+        else:
+            print('subscription failed', err_message)
+    quote_ctx.close()
+        
+    return numberofreset
+    
 remain_hkstocks = checkstatus()
 snapshot = get_snapshot(remain_hkstocks)
 final = pd.concat(snapshot)
+getstock = myfilter(final)
+getstock2 = comparesma250(getstock)
 top_10_trate = final.nlargest(10, 'turnover_rate')
 top_10_am = final.nlargest(10, 'amplitude')
-                        
